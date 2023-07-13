eacdd5a55d00862f7426c7abc71a7a6839f1f78d
Juan Diego Campos Neira <juandiego.camposneira@endava.com>
2023-07-13 10:23:18 -0500

    Linter and prettier installed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,10 +21,12 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
   },
-}
+};

b22908ba3aa06437007fbf0a59f3c5f9ed56c3b9
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2023-07-13 16:26:51 -0500

    MUI setup done (#4)
    [Feature/setup-mui-and-tailwind] Mui setup done, tailwind won't be used

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,22 +3,22 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ["airbnb", "airbnb/hooks", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: ['prettier', 'airbnb', 'airbnb/hooks'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ["react-refresh"],
+  plugins: ['react-refresh'],
   rules: {
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts"] }],
-    "react-refresh/only-export-components": [
-      "warn",
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-non-null-assertion": "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
 
 export default function App() {
   useEffect(() => {
-    console.log("test");
+    console.log('test');
   }, []);
 
   const [count, setCount] = useState<number>(0);
 
   return (
     <>
-      <h1>Pitchers</h1>
+      <Typography variant="h1">Pitchers</Typography>
+      <Typography variant="h1">click</Typography>
+      <Typography variant="body1">click</Typography>
       <button type="button" onClick={() => setCount(count + 1)}>
         click me
       </button>
+      <Button variant="contained">Hello World</Button>
+      <Button variant="contained" color="secondary">
+        Hello World
+      </Button>
     </>
   );
 }

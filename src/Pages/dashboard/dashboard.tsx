442b36013b60161d259e22bf09c8fa18f6ea97bc
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2023-07-22 09:12:09 -0500

    Feature/create routes (#9)
    * Router installed
    
    * [feature/create-routes] Routes created and router configurated

---
+++ src/Pages/dashboard/dashboard.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  // How could we type the search params?
+  // This should be used
+  // const [searchParams, setSearchParams] = useSearchParams();
+
+  return <h1> Dashboard page </h1>;
+}
---
+++ src/Router/routes.tsx
@@ -0,0 +1,16 @@
+import { lazy } from 'react';
+import { RouteObject, Route, createRoutesFromElements } from 'react-router-dom';
+
+const Dashboard = lazy(() => import('../Pages/dashboard/dashboard.tsx'));
+const Login = lazy(() => import('../Pages/login/login.tsx'));
+const Property = lazy(() => import('../Pages/property/property.tsx'));
+
+const CustomRoutes: RouteObject[] = createRoutesFromElements(
+  <>
+    <Route path="/login" element={<Login />} />
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/property/:propertyId" element={<Property />} />
+  </>,
+);
+
+export default CustomRoutes;

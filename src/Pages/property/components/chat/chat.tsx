e0dae7547a5872a2cfc2b2b6ee4bd568a49a908a
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2023-08-08 13:16:39 -0500

    Feature/chat (#25)
    * Logo path changed and created section for property detailed
    
    * Missing code refactor of this
    
    * chat component separated
    
    * scrollable fixed

---
+++ src/Pages/property/components/chat/chat.tsx
@@ -0,0 +1,104 @@
+import { useCallback, useContext, useState } from 'react';
+import {
+  CardHeader,
+  CardContent,
+  Typography,
+  Card,
+  Stack,
+} from '@mui/material';
+import { useParams } from 'react-router-dom';
+import Loading from '../../../../Shared/loading.tsx';
+import { Scrollable } from '../../../../Shared/scrollable/scrollable.styled.ts';
+import { GrowTransition } from '../../../../Shared/transitions/index.ts';
+import Layout from '../../../../Shared/layout/layout.styles.ts';
+import SearchComponent from '../../../../Shared/search/index.ts';
+import PitchersIcon from '../../../../Assets/pitchers.icon.tsx';
+import {
+  ChatMessageProps,
+  ChatResponse,
+  Message,
+  QueryMessage,
+} from './chat.types.ts';
+import ChatMessage from './chat.styled.ts';
+import {
+  DefaultSearchValues,
+  SearchContext,
+  TSearchContext,
+} from '../../../../Contexts/search/index.ts';
+import { API, HttpClient } from '../../../../Network/index.ts';
+
+export const DefaultChatMessages: Array<ChatMessageProps> = [
+  new Message({
+    message: 'Hello there, how can I assist you today?',
+    isResponse: true,
+    timeout: 1000,
+  }),
+];
+
+export default function Chat() {
+  const { propertyId } = useParams<string>();
+  const { searchContext, setSearchContext } =
+    useContext<TSearchContext>(SearchContext);
+  const [chatMessages, setChatMessages] =
+    useState<ChatMessageProps[]>(DefaultChatMessages);
+  const chatHttpClient = useCallback(
+    () => new HttpClient<QueryMessage, ChatResponse>(API.CHAT_API),
+    [],
+  );
+
+  const handleSearch = async () => {
+    setSearchContext({ ...searchContext, loading: true });
+    const tempChatMessages: ChatMessageProps[] = [...chatMessages].concat([
+      new Message({ message: searchContext.input }),
+      new Message({ loading: true, timeout: 1000, isResponse: true }),
+    ]);
+    setChatMessages(tempChatMessages);
+    try {
+      const searchBody: QueryMessage = {
+        message: searchContext.input,
+      };
+      const { response }: ChatResponse = await chatHttpClient().post(
+        searchBody,
+        {
+          url: '2c055ae9-8b30-4484-b1b6-283055be1242' || propertyId,
+        },
+      );
+      const tempChat = [...tempChatMessages.slice(0, -1)].concat([
+        new Message({ message: response, isResponse: true }),
+      ]);
+      setChatMessages(tempChat);
+    } catch (error) {
+      setSearchContext({
+        ...DefaultSearchValues,
+        error: 'Something went wrong, try again',
+      });
+      return;
+    }
+    setSearchContext(DefaultSearchValues);
+  };
+
+  return (
+    <Layout layoutdirection="column" height='100%' sx={{ flex: '1', padding: '0' }}>
+      <Scrollable flex="1" rowGap="1.3rem" sx={{ padding: '0' }}>
+        {chatMessages.map((message) => (
+          <GrowTransition timeout={message.timeout} key={message.id}>
+            <ChatMessage key={message.id}>
+              <CardHeader
+                avatar={message.isResponse && <PitchersIcon size="icon" />}
+                subheader={message.time}
+              />
+              <CardContent>
+                {message.loading ? (
+                  <Loading />
+                ) : (
+                  <Typography>{message.message}</Typography>
+                )}
+              </CardContent>
+            </ChatMessage>
+          </GrowTransition>
+        ))}
+      </Scrollable>
+      <SearchComponent handleSearch={handleSearch} />
+    </Layout>
+  );
+}
--- src/Pages/property/property.tsx
+++ src/Pages/property/property.tsx
@@ -1,37 +1,19 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import {
-  Box,
-  Button,
-  Card,
-  Chip,
-  Stack,
-  Typography,
-  styled,
-} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Chip, Stack, Typography } from '@mui/material';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import Layout from '../../Shared/layout/layout.styles.ts';
 import { Scrollable } from '../../Shared/scrollable/scrollable.styled.ts';
 import { PitchersLogo } from '../../Assets/index.ts';
-
-export const Section = styled(Card)({
-  flex: '1',
-});
+import StyledSection from './components/section.styled.ts';
+import Chat from './components/chat/chat.tsx';
 
 export default function Property() {
-  const { propertyId } = useParams<string>();
-
   const navigation = useNavigate();
   const handleGoBack = () => navigation(-1);
 
   return (
     // TODO: Extract the header into a separate component
-    <Layout
-      layoutdirection="column"
-      disableGutters
-      sx={{
-        alignItems: 'flex-start',
-      }}
-    >
+    <Layout layoutdirection="column" disableGutters>
       <Stack direction="row" width="100%">
         <Button
           variant="text"
@@ -49,11 +31,18 @@ export default function Property() {
           <PitchersLogo size="small" />
         </Box>
       </Stack>
-      <Stack direction={{ lg: 'row', sm: 'column' }} gap="1rem" flex="1">
-        <Section>
-          <Typography variant="h1">{propertyId}</Typography>
-        </Section>
-        <Section>
+      <Stack
+        direction={{ lg: 'row', sm: 'column' }}
+        gap="1rem"
+        flex="1"
+        padding="1rem"
+        maxHeight="95%"
+        paddingBottom="0"
+      >
+        <StyledSection>
+          <Typography variant="h1">Test</Typography>
+        </StyledSection>
+        <StyledSection>
           <Layout layoutdirection="column" height="100%">
             <Typography variant="h3">Selling pitch</Typography>
             <Stack
@@ -73,14 +62,30 @@ export default function Property() {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
                 eius facilis autem. Eveniet inventore perspiciatis hic. Placeat
                 incidunt ipsum et cupiditate accusamus dolores exercitationem.
-                Iure est iste quibusdam eos cumque?
+                Iure est iste quibusdam eos cumque? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Laudantium earum sapiente, et
+                error accusamus, inventore explicabo harum culpa doloremque,
+                eveniet excepturi ab. Voluptate veritatis perspiciatis Placeat
+                incidunt ipsum et cupiditate accusamus dolores exercitationem.
+                Iure est iste quibusdam eos cumque? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Laudantium earum sapiente, et
+                error accusamus, inventore explicabo harum culpa doloremque,
+                eveniet excepturi ab. Voluptate veritatis perspiciatis Placeat
+                incidunt ipsum et cupiditate accusamus dolores exercitationem.
+                Iure est iste quibusdam eos cumque? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Laudantium earum sapiente, et
+                error accusamus, inventore explicabo harum culpa doloremque,
+                eveniet excepturi ab. Voluptate veritatis perspiciatis Placeat
+                incidunt ipsum et cupiditate accusamus dolores exercitationem.
+                Iure est iste quibusdam eos cumque? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Laudantium earum sapiente, et
+                error accusamus, inventore explicabo harum culpa doloremque,
+                eveniet excepturi ab. Voluptate veritatis perspiciatis
               </Typography>
             </Scrollable>
           </Layout>
-        </Section>
-        <Section>
-          <Typography variant="h1">chat</Typography>
-        </Section>
+        </StyledSection>
+        <Chat />
       </Stack>
     </Layout>
   );

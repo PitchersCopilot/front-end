4f92b39ea6f93c82ba62a8b4fa5fabb41c85470e
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2023-08-08 13:16:39 -0500

    Feature/chat (#25)
    * Logo path changed and created section for property detailed
    
    * Missing code refactor of this
    
    * chat component separated
    
    * scrollable fixed

--- src/Network/api.ts
+++ src/Network/api.ts
@@ -8,5 +8,5 @@ export const API: APIs = {
   SEARCH_API:
     'https://apptestwithstorage.azurewebsites.net/api/fetchDataProcessingQueryResult',
   CHAT_API:
-    'http://52.179.114.160:3000/chat/c45b1544-9fd6-4189-9fd8-2064ccaf971b',
+    'http://20.253.126.249:3000/chat/',
 };
---
+++ src/Pages/property/components/section.styled.ts
@@ -0,0 +1,7 @@
+import { styled, Card } from '@mui/material';
+
+const StyledSection = styled(Card)({
+  flex: '1',
+});
+
+export default StyledSection;
